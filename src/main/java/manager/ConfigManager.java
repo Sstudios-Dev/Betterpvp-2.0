4971f72299c5cd372ec370df5f24b6f3dcd82452
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-01-19 23:01:10 -0600

    Update ConfigManager.java

--- src/main/java/manager/ConfigManager.java
+++ src/main/java/manager/ConfigManager.java
@@ -1,13 +1,26 @@
+/**
+ * Class responsible for managing the configuration of the BetterPvP plugin.
+ * This class handles the initialization and provides access to the plugin's configuration.
+ */
 package manager;
 
 import github.betterpvp.BetterPvP;
 import org.bukkit.configuration.file.FileConfiguration;
 
+/**
+ * Class providing methods to configure and access the plugin's configuration.
+ */
 public class ConfigManager {
 
+    // Plugin configuration stored in a FileConfiguration object
     private static FileConfiguration config;
 
-    public static void setupConfig(BetterPvP betterPvP){
+    /**
+     * Configures the FileConfiguration instance for the BetterPvP plugin.
+     * @param betterPvP Instance of the BetterPvP plugin.
+     */
+    public static void setupConfig(BetterPvP betterPvP) {
+        // Set the plugin configuration with the default configuration if it does not exist
         ConfigManager.config = betterPvP.getConfig();
         betterPvP.saveDefaultConfig();
     }
