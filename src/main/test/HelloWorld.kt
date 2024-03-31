dc77d21c2c2d47ec98fb58c3ad47bd17f2668c78
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-03-31 00:16:25 -0600

    HelloWorld in kotlin

--- src/main/test/HelloWorld.kt
+++ src/main/test/HelloWorld.kt
@@ -1,5 +1,6 @@
 package com.example.helloworld
 
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -13,15 +14,32 @@ class HelloWorld : JavaPlugin() {
         getCommand("hello")?.setExecutor(HelloCommand())
     }
 
-    class HelloCommand : CommandExecutor {
+    inner class HelloCommand : CommandExecutor {
         override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
             // Check if the command was executed by a player
             if (sender is Player) {
-                // Send a "Hello World" message to the player
-                sender.sendMessage("Hello World!")
-                return true
+                // Check if the player has permission to use the command
+                if (sender.hasPermission("helloworld.greet")) {
+                    // Check if any arguments were provided
+                    if (args.isNotEmpty()) {
+                        val message = args.joinToString(" ")
+                        val formattedMessage = formatMessage(message)
+                        sender.sendMessage(formattedMessage)
+                    } else {
+                        sender.sendMessage("Usage: /hello <message>")
+                    }
+                } else {
+                    sender.sendMessage("You do not have permission to use this command.")
+                }
+            } else {
+                sender.sendMessage("This command can only be executed by players.")
             }
-            return false
+            return true
+        }
+
+        private fun formatMessage(message: String): String {
+            // Apply color and format to the message
+            return ChatColor.translateAlternateColorCodes('&', message)
         }
     }
 }
